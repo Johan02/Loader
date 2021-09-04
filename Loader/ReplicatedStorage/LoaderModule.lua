868633fbad6f9e77ebea54a1d38ea99e720f7405
Johan02 <johandif3@gmail.com>
2021-09-04 13:26:42 +0200

    LoadOut function Created

---
+++ Loader/ReplicatedStorage/LoaderModule.lua
@@ -0,0 +1,16 @@
+local LoaderModule = {}
+
+local LoadedOutModels = Instance.new('Folder', game.ReplicatedStorage)
+LoadedOutModels.Name = 'LoadedOutModels'
+
+
+function LoaderModule.LoadOut(Model)
+
+    if Model.PrimaryPart then
+        Model.Parent = LoadedOutModels
+    end
+
+end
+
+
+return LoaderModule
