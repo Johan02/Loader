e82e36c006e189d92e5606ddeb96b064403b0691
Johan02 <johandif3@gmail.com>
2021-09-21 01:01:45 +0200

    Huge update

--- Loader/ReplicatedStorage/Loader.lua
+++ Loader/ReplicatedStorage/Loader.lua
@@ -8,23 +8,65 @@ local HumanoidRootPart
 local ModelsVisible = {}
 local ModelsPosition = {}
 local Models = {}
-
+local LastModelLoaded = 0
 
 
 
 for indexModel, realModel in pairs(MainModelZX:GetDescendants()) do
-    if realModel:IsA('Model') then
-        Models[indexModel] = realModel
-        ModelsVisible[indexModel] = true
+	if realModel:IsA('Model') then
+		Models[indexModel] = realModel
+		ModelsVisible[indexModel] = true
 
-        local realModelPosition = realModel:GetModelCFrame().Position
-	    ModelsPosition[indexModel] = realModelPosition
-    end
+		local realModelPosition = realModel:GetModelCFrame().Position
+		ModelsPosition[indexModel] = {realModelPosition.X, realModelPosition.Y, realModelPosition.Z}
+	end
 end
 
+print(ModelsPosition)
 
+local function GetDistance()
+	if not HumanoidRootPart then return	end
+	for indexModel, modelPosition in pairs(ModelsPosition) do
+		local realModelPosition = Vector3.new(modelPosition[1],modelPosition[2],modelPosition[3] )
+		if (HumanoidRootPart.Position - realModelPosition).Magnitude > 50 then
+			--print(indexModel)
+			return indexModel, false
+		elseif (HumanoidRootPart.Position - realModelPosition).Magnitude <= 50 then
+			return indexModel, true
+		end
+	end
+	
+	
+	
+end
 
-local function Loadout()
+local function Loadout(realIndexModel, Visible)
+	for indexModel, realModel in pairs(Models) do
+		if realIndexModel == indexModel then
+			for indexVisibleModel, visibleModel in pairs(ModelsVisible) do
+				if indexVisibleModel == realIndexModel then
+					
+					
+					if Visible == true then
+						ModelsVisible[realIndexModel] = Visible
+						Models[realIndexModel].Parent = MainModelZX
+					elseif Visible == false then
+						ModelsVisible[realIndexModel] = Visible
+						Models[realIndexModel].Parent = nil
+					end
+					
+					
+					--[[if ModelsVisible[realIndexModel] == true then
+						
+						ModelsVisible[realIndexModel] = false
+						Models[realIndexModel].Parent = nil
+						
+					
+					end]]
+				end
+			end
+		end
+	end
 
 end
 
@@ -40,11 +82,17 @@ Player.CharacterAdded:Connect(CharacterAdded)
 
 while true do
 
-    wait()
+	local DistanceConfirmedAndIndexModel, VisibleStatus = GetDistance()
+	
+	if HumanoidRootPart and DistanceConfirmedAndIndexModel and LastModelLoaded ~= DistanceConfirmedAndIndexModel then
+		print(LastModelLoaded)
+		print(DistanceConfirmedAndIndexModel)
+		LastModelLoaded = DistanceConfirmedAndIndexModel
+		Loadout(DistanceConfirmedAndIndexModel,VisibleStatus)
+	end
+	wait()
+
 end
-
-
-
 
 
 
