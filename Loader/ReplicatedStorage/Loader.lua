08b0605d79a055a4c1007b65137ad337eb8b04c9
Johan02 <johandif3@gmail.com>
2021-09-21 00:15:44 +0200

    Basic Functions

--- Loader/ReplicatedStorage/Loader.lua
+++ Loader/ReplicatedStorage/Loader.lua
@@ -1,3 +1,51 @@
 local Player = game:GetService('Players').LocalPlayer
 
 local MainModelZX = workspace:WaitForChild('MainModelZX')
+
+
+local HumanoidRootPart
+
+local ModelsVisible = {}
+local ModelsPosition = {}
+local Models = {}
+
+
+
+
+for indexModel, realModel in pairs(MainModelZX:GetDescendants()) do
+    if realModel:IsA('Model') then
+        Models[indexModel] = realModel
+        ModelsVisible[indexModel] = true
+
+        local realModelPosition = realModel:GetModelCFrame().Position
+	    ModelsPosition[indexModel] = realModelPosition
+    end
+end
+
+
+
+local function Loadout()
+
+end
+
+local function CharacterAdded(Character)
+	if Character then
+		HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+	end
+end
+
+CharacterAdded(Player.Character)
+Player.CharacterAdded:Connect(CharacterAdded)
+
+
+while true do
+
+    wait()
+end
+
+
+
+
+
+
+
